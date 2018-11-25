ca6a051af411888120f554a23d35fc1e58c04948
want <wantbycre@gmail.com>
2018-11-25 16:41:05 +0900

    ts styled setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
 import client from "./apollo";
-import App from "./App";
+import App from "./Components/App";
+import GlobalStyle from "./global-styles";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
+    <GlobalStyle />
   </ApolloProvider>,
   document.getElementById("root") as HTMLElement
 );
